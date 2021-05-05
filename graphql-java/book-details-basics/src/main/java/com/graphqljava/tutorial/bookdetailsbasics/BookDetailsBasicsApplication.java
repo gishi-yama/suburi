48cbbf3a1fac703985c5718c210ceeeea8a91b9e
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-05-05 18:03:12 +0900

    graphqlの標準プロジェクトを追加

---
+++ graphql-java/book-details-basics/src/main/java/com/graphqljava/tutorial/bookdetailsbasics/BookDetailsBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookdetailsbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDetailsBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDetailsBasicsApplication.class, args);
+	}
+
+}
---
+++ graphql-java/book-details-basics/src/test/java/com/graphqljava/tutorial/bookdetailsbasics/BookDetailsBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookdetailsbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDetailsBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
